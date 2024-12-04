31de100a91ceab8762f23b3b1c5ed6c13fe83a76
Viacheslau Bulbochka <disablemon@gmail.com>
2024-12-04 16:58:22 +0300

    Cry

--- dotnet/QR-Code-generator/obj/Debug/net8.0-windows/QR-Code-generator.AssemblyInfo.cs
+++ dotnet/QR-Code-generator/obj/Debug/net8.0-windows/QR-Code-generator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QR-Code-generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07ef844955f95033889001a87353821f91ac515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eecd72a9267656707de3c15963fcfb74770967b")]
 [assembly: System.Reflection.AssemblyProductAttribute("QR-Code-generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QR-Code-generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
